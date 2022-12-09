d2ea5b37d1ed6c2a250140055a89da92624c07b2
Anmar Homod <eng.anmarhomod@gmail.com>
2022-12-09 22:34:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,21 +6,16 @@ from time import gmtime, strftime
 import twilio
 from twilio.rest import Client
 
-# twilio credentials
 acc_sid = ""
 auth_token = ""
 client = Client(acc_sid, auth_token)
 
-# firebase credentials
-# key.json is your certificate of firebase project
 cred = credentials.Certificate("key.json")
 default_app = initialize_app(cred)
 db = firestore.client()
 database_reference = db.collection("on_call")
 
-# Here the collection name is on_call which has documents with fields phone , from (%H:%M:%S time to call the person),date
 
-# gets data from cloud database and calls 5 min prior the time (from time) alloted in the database
 def search():
 
     calling_time = datetime.now()
